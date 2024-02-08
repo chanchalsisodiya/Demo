c33e2d53545f15e41d49e4e942aac389da184143
chanchal <chanchal.rajput1301@gmail.com>
2024-02-08 12:28:12 +0530

    first

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+class Student {
+    // data member (also instance variable)
+    int id;
+    // data member (also instance variable)
+    String name;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String args[])
+    {
+        // creating an object of
+        // Student
+        Student s1 = new Student();
+        System.out.println(s1.id);
+        System.out.println(s1.name);
     }
 }
